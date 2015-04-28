faa317a74e16f4b0a7c6afe28e59c92ea73a7240
Marta <mattifie@gmail.com>
2015-04-28 08:53:56 -0500

    first push

--- config.rb
+++ config.rb
@@ -7,6 +7,8 @@ css_dir = "stylesheets"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
+fonts_dir = "fonts"
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
